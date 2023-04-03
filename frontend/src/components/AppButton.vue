93478fcd787f2fe778addd83b825795fffc16e73
Joe Joiner <2454320+joejoinerr@users.noreply.github.com>
2023-04-03 09:52:25 +0100

    Add header components

---
+++ frontend/src/components/AppButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+  defineProps(['buttonClass'])
+</script>
+
+<template>
+  <button class="block  px-4  py-3  leading-4" :class="buttonClass">
+    <slot></slot>
+  </button>
+</template>
---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import AppButton from './AppButton.vue'
+</script>
+
+<template>
+  <header class="max-w-screen-lg  flex  justify-between  items-center  mx-auto  px-4  py-4  mb-8">
+    <div class="text-3xl  font-bold">Cooper</div>
+    <AppButton buttonClass="bg-gray-200">+ New</AppButton>
+  </header>
+</template>
