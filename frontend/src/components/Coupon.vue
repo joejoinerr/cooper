aa813587d3c97d7fab4afaf88f24bfffc1b4d234
Joe Joiner <2454320+joejoinerr@users.noreply.github.com>
2023-04-03 09:52:45 +0100

    Add coupon list

---
+++ frontend/src/components/Coupon.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mb-8  border  border-black">
+    <div class="px-4  py-2  bg-orange-500"><!-- Brand banner -->
+      <slot name="brand-name"></slot>
+    </div>
+    <div class="py-5  text-center  text-2xl"><!-- Code section -->
+      <slot name="coupon-code"></slot>
+    </div>
+    <div class="px-4  py-2  bg-orange-500"><!-- Expiry banner -->
+      Expires: <slot name="expiry-date"></slot>
+    </div>
+  </div>
+</template>
---
+++ frontend/src/components/CouponList.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+import Coupon from './Coupon.vue';
+
+async function fetchAllCoupons() {
+  const res = await fetch('/api/coupons');
+  return await res.json();
+}
+
+const couponList = ref([]);
+fetchAllCoupons().then((response) => {
+  couponList.value = response.coupons
+  console.log(couponList.value)
+})
+</script>
+
+<template>
+  <div class="px-4">
+    <template v-if="couponList.length">
+      <Coupon v-for="coupon in couponList">
+        <template #brand-name>
+          {{ coupon.brand }}
+        </template>
+        <template #coupon-code>
+          {{ coupon.code }}
+        </template>
+        <template #expiry-date>
+          {{ coupon.expiry }}
+        </template>
+      </Coupon>
+    </template>
+    <div v-else>
+      No coupons yet :)
+    </div>
+  </div>
+</template>
